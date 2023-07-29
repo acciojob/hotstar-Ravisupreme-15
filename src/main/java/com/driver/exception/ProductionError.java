f2d9617c49722054d3a37cabcf827455336629a0
chadraMohanRao <supremeravi1819@gmail.com>
2023-07-29 23:20:43 +0530

    commited new changes

---
+++ src/main/java/com/driver/exception/ProductionError.java
@@ -0,0 +1,8 @@
+package com.driver.exception;
+
+public class ProductionError extends Exception{
+
+    public ProductionError(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -1,6 +1,8 @@
 package com.driver.services;
 
 import com.driver.EntryDto.WebSeriesEntryDto;
+import com.driver.Transformer.WebSeriesTransformer;
+import com.driver.exception.ProductionError;
 import com.driver.model.ProductionHouse;
 import com.driver.model.WebSeries;
 import com.driver.repository.ProductionHouseRepository;
@@ -8,23 +10,59 @@ import com.driver.repository.WebSeriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class WebSeriesService {
+import java.util.List;
+import java.util.Optional;
 
-    @Autowired
-    WebSeriesRepository webSeriesRepository;
 
-    @Autowired
-    ProductionHouseRepository productionHouseRepository;
+    @Service
+    public class  WebSeriesService{
 
-    public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception{
+        @Autowired
+        WebSeriesRepository webSeriesRepository;
 
-        //Add a webSeries to the database and update the ratings of the productionHouse
-        //Incase the seriesName is already present in the Db throw Exception("Series is already present")
-        //use function written in Repository Layer for the same
-        //Dont forget to save the production and webseries Repo
+        @Autowired
+        ProductionHouseRepository productionHouseRepository;
+        public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception, ProductionError {
 
-        return null;
-    }
+            //Add a webSeries to the database and update the ratings of the productionHouse
+            //Incase the seriesName is already present in the Db throw Exception("Series is already present")
+            //use function written in Repository Layer for the same
+            //Dont forget to save the production and webseries Repo
+
+            WebSeries webSeries = WebSeriesTransformer.convertDtoToEntity(webSeriesEntryDto);
+            WebSeries checkSeries = webSeriesRepository.findBySeriesName(webSeries.getSeriesName());
+            if(checkSeries != null){
+                throw new Exception("Series is already present");
+            }
+            int id = webSeriesEntryDto.getProductionHouseId();
+            Optional<ProductionHouse> productionHouseOpt = productionHouseRepository.findById(id);
+            ProductionHouse productionHouse = productionHouseOpt.get();
+            if(productionHouse == null){
+                throw new ProductionError("Production house is not present");
+            }
+
+            webSeries.setProductionHouse(productionHouse);
+            webSeries = webSeriesRepository.save(webSeries);
+
+            List<WebSeries> seriesList = productionHouse.getWebSeriesList();
+            seriesList.add(webSeries);
+
+            double productionHouseRating = 0;
+            for(WebSeries series : seriesList){
+                productionHouseRating += series.getRating();
+            }
+            int seriesCount = seriesList.size();
+            productionHouseRating = productionHouseRating / seriesCount;
+
+            productionHouse.setRatings(productionHouseRating);
+
+            productionHouseRepository.save(productionHouse);
+
+            return webSeries.getId();
+        }
+
+
+
+
 
 }
